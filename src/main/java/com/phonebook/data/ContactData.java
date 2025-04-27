42b36500a63776312715f7684ddb743630851deb
natal <nataly.sib@gmail.com>
2025-04-27 18:45:14 +0200

    Version1

---
+++ src/main/java/com/phonebook/data/ContactData.java
@@ -0,0 +1,10 @@
+package com.phonebook.data;
+
+public class ContactData {
+    public static final String NAME = "My_nameMy";
+    public static final String LAST_NAME = "My_lastname";
+    public static final String PHONE = "01234567890";
+    public static final String EMAIL = "qqq@qqq.com";
+    public static final String ADDRESS = "My_address";
+    public static final String DESCRIPTION = "My_disctiption";
+}
---
+++ src/main/java/com/phonebook/data/UserData.java
@@ -0,0 +1,6 @@
+package com.phonebook.data;
+
+public class UserData {
+    public static final String EMAIL = "qwe@asd.com";
+    public static final String PASSWORD = "ABSabs123!";
+}
