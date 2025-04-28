a9dea679240cde9253d8b31cf3bf227631880217
natal <nataly.sib@gmail.com>
2025-04-28 12:32:41 +0200

    Version1

--- src/main/java/com/phonebook/fw/ApplicationManager.java
+++ src/main/java/com/phonebook/fw/ApplicationManager.java
@@ -1,9 +1,14 @@
 package com.phonebook.fw;
 
+import com.phonebook.utils.MyListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringDecorator;
+import org.openqa.selenium.support.events.WebDriverListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 
@@ -11,6 +16,9 @@ public class ApplicationManager {
 
     String browser;
     WebDriver driver;
+
+    Logger logger = LoggerFactory.getLogger(ApplicationManager.class);
+    //snova imenno biblioteka slf4j
 
     UserHelper user;
     ContactHelper contact;
@@ -23,12 +31,19 @@ public class ApplicationManager {
     public void init() {
         if (browser.equalsIgnoreCase("chrome")) {
             driver = new ChromeDriver();
+            logger.info("Test starts in Chrome browser ");
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
+            logger.info("Test starts in Edge browser ");
         } else if (browser.equalsIgnoreCase("firefox")) {
             driver = new FirefoxDriver();
+            logger.info("Test starts in Firefox browser ");
         }
+        WebDriverListener listener = new MyListener();
+        driver = new EventFiringDecorator<>(listener).decorate(driver);
+
         driver.get("https://telranedu.web.app/home");
+        logger.info("Current url --> " + driver.getCurrentUrl());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
